81ebf35d14bf81a529786e1a0213c8a5ca538e92
vanya-rgb <77493592+vanya-rgb@users.noreply.github.com>
2021-06-22 12:57:40 +0500

    new header

--- myblog-root/myblog/settings.py
+++ myblog-root/myblog/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'myblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['B:\\Git_\\Django_text\\myblog-root\\myblog\\template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
