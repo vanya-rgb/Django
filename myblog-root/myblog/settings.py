39944e48855a28d9253121c37b2c709db7d3ea05
vanya-rgb <77493592+vanya-rgb@users.noreply.github.com>
2021-06-22 18:21:27 +0500

    add POST model

--- myblog-root/myblog/settings.py
+++ myblog-root/myblog/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'events.apps.EventsConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
