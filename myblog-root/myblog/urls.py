135f77f16bc2383439497354672333e98c060cb3
vanya-rgb <77493592+vanya-rgb@users.noreply.github.com>
2021-06-24 03:13:46 +0500

    some shit

--- myblog-root/myblog/urls.py
+++ myblog-root/myblog/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -9,5 +9,6 @@ import events.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', events.views.home, name='home'),
+    path('blog/', include('blog.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
