7169496e38e4ce3d0b5a9d7b241d3601da3134d5
vanya-rgb <77493592+vanya-rgb@users.noreply.github.com>
2021-06-28 22:48:37 +0500

    finish

--- myblog-root/blog/models.py
+++ myblog-root/blog/models.py
@@ -5,5 +5,12 @@ class Post(models.Model):
 	date = models.DateTimeField()
 	text = models.TextField()
 	image = models.ImageField(upload_to='event_images/')
+
+	def get_summary(self):
+		return self.text[:150]
+
+
+	def __str__(self):
+		return self.title
 	
 	
--- mystore-root/mystore/wsgi.py
+++ mystore-root/mystore/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for text project.
+WSGI config for mystore project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'text.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mystore.settings')
 
 application = get_wsgi_application()
